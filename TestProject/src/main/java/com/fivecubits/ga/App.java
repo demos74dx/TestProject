45502b01edada47f5b1f680fe5fcdc457b715138
ColeG24 <cole.gordon57@gmail.com>
2016-05-27 15:07:16 -0600

    Add files via upload

---
+++ TestProject/src/main/java/com/fivecubits/ga/App.java
@@ -0,0 +1,14 @@
+package com.fivecubits.ga;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        DataClass dc= new DataClass();
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ TestProject/src/main/java/com/fivecubits/ga/DataClass.java
@@ -0,0 +1,13 @@
+package com.fivecubits.ga;
+
+
+import lombok.Data;
+
+/**
+ * Created by Cole on 5/27/2016.
+ */
+@Data
+public class DataClass {
+    private String word;
+    private int num;
+}
